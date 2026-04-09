f0ecfc512a81f953ffb3a6bdf9c2726101ac5b45
Nsengiyunva <kingbecks07@gmail.com>
2026-04-09 12:11:28 +0300

    updates the ncdc eshop

--- src/features/auth/components/RegisterTin.jsx
+++ src/features/auth/components/RegisterTin.jsx
@@ -58,7 +58,7 @@ export const RegisterTin = () => {
             to="/auth/login"
             className="font-medium text-green-600 hover:text-green-500"
           >
-            Login instaed
+            Login instead
           </Link>
         </div>
       </div>
